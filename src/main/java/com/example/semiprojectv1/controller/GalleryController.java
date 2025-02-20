9eb7ff1f52e0c8a3fe1d4e6d95deb20b1434fbed
leegwangje <ze105@naver.com>
2025-02-20 16:52:46 +0900

    하루 끝

---
+++ src/main/java/com/example/semiprojectv1/controller/GalleryController.java
@@ -0,0 +1,15 @@
+package com.example.semiprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+
+    @GetMapping("/list")
+    public String index() {
+        return "views/gallery/list";
+    }
+}
