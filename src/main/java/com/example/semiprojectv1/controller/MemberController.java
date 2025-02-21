f56c1ea7df809b115453984590c7f9fd75c1e9ab
Kwang je <gwangjae0105@gmail.com>
2025-02-21 14:35:04 +0900

    MemberController 테스트 Member 클래스 생성

--- src/main/java/com/example/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.example.semiprojectv1.controller;
 
 import com.example.semiprojectv1.domain.MemberDTO;
+import com.example.semiprojectv1.repository.MemberRepository;
+import com.example.semiprojectv1.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/join")
     public String join() {
@@ -30,9 +36,15 @@ public class MemberController {
 
     @PostMapping ("/join")
     public String joinook(MemberDTO member) {
+
+        String returnPage = "redirect:/member/error";
+
         log.info("submit된 회원 정보 : {}", member);
 
-        return "redirect:/member/login";
+        if(memberService.newMember(member))
+            returnPage = "redirect:/member/login";
+
+        return returnPage;
     }
 
 }
