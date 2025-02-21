7e80d7775a4273224e0915d27abb2b61caa98ed0
Kwang je <gwangjae0105@gmail.com>
2025-02-21 11:51:17 +0900

    MemberRepository 작성 MemberRepository 테스트 작성 MemberDTO builder 패턴 추가

--- src/main/java/com/example/semiprojectv1/domain/MemberDTO.java
+++ src/main/java/com/example/semiprojectv1/domain/MemberDTO.java
@@ -1,8 +1,10 @@
 package com.example.semiprojectv1.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MemberDTO {
 
     private String userid;
---
+++ src/test/java/com/example/semiprojectv1/member/MemberMapperTest.java
@@ -0,0 +1,38 @@
+package com.example.semiprojectv1.member;
+
+import com.example.semiprojectv1.domain.MemberDTO;
+import com.example.semiprojectv1.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@MybatisTest
+public class MemberMapperTest {
+
+    @Autowired
+    private MemberRepository memberMapper;  // final 제거
+
+    @Test
+    @DisplayName("MemberMapper Insert test")
+    void insertMember() {
+        // Given : 테스트에 사용할 데이터 제공
+        MemberDTO dto = MemberDTO.builder()
+                .userid("abc1234")
+                .passwd("987xyz")
+                .name("abc123")
+                .email("abc123@gmail.com")
+                .build();
+
+        // When : 테스트로 테스트할 기능 호출
+        int result = memberMapper.insertMember(dto);
+
+        // Then : 호출되고 난 후 결과값 확인
+        log.info("result: {}", result);
+        assertThat(result).isEqualTo(1);
+    }
+}
