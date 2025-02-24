4d6f6dedcc23bca13a1d8f86e4ff9aaa0f41773d
Kwang je <gwangjae0105@gmail.com>
2025-02-24 12:49:05 +0900

    fetch를 이용한 회원가입 기능 구현 4 비밀번호에 대해 SHA-256 해시 상성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- members 테이블
 INSERT INTO members (userid, passwd, name, email)
-VALUES ('abc123', 'abc123', 'abc123', 'abc123@abc.co.kr');
+VALUES ('abc123', '6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090', 'abc123', 'abc123@abc.co.kr');
 
 INSERT INTO members (userid, passwd, name, email)
-VALUES ('987xyz', '987xyz', '987xyz', '987xyz@987.co.kr');
+VALUES ('987xyz', 'c7d0cccedf3900d458784c9fba1336d71c434bc380474cc219fda24a2d420f8f', '987xyz', '987xyz@987.co.kr');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table members(
     mno int auto_increment primary key,
     userid varchar(18) unique not NULL ,
-    passwd varchar(18)  not NULL ,
+    passwd varchar(64)  not NULL ,
     name varchar(50) not null,
     email varchar(100) not null,
     regdate datetime default current_timestamp
