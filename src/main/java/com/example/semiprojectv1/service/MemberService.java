4a71b5444b13a25b7354a5f6fb2af00c710732ce
Kwang je <gwangjae0105@gmail.com>
2025-02-25 10:42:08 +0900

    로그인 기능 구현 2 - loginok 메서드 구현, loginMember 메서드 구현, HttpSession으로 로그인사용자 정보 유지

--- src/main/java/com/example/semiprojectv1/service/MemberService.java
+++ src/main/java/com/example/semiprojectv1/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.semiprojectv1.service;
 
+import com.example.semiprojectv1.domain.Member;
 import com.example.semiprojectv1.domain.MemberDTO;
 import com.example.semiprojectv1.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,14 @@ public class MemberService {
         int result = memberMapper.insertMember(member);
         return result == 1;     // 회원정보가 테이블 저장되었느지 여부에 따라
                                 // true, false 반환
+    }
+
+    public Member loginMember(MemberDTO member) {
+        Member findMember = memberMapper.findByUserid(member.getUserid());
+
+        if(findMember==null || ! findMember.getPasswd().equals(member.getPasswd())){
+            throw new IllegalStateException("아이디나 비밀번호가 일치하지 않습니다");
+        } return findMember;
     }
 
 }
