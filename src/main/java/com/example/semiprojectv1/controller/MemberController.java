b4e8256ebf9980f355988b7897a8f8157795f3db
leegwangje <ze105@naver.com>
2025-02-25 10:42:08 +0900

    로그인 기능 구현 2 - loginok 메서드 구현, loginMember 메서드 구현, HttpSession으로 로그인사용자 정보 유지

--- src/main/java/com/example/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.semiprojectv1.controller;
 
+import com.example.semiprojectv1.domain.Member;
 import com.example.semiprojectv1.domain.MemberDTO;
 import com.example.semiprojectv1.repository.MemberRepository;
 import com.example.semiprojectv1.service.MemberService;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 @RequestMapping("/member")
@@ -51,6 +55,34 @@ public class MemberController {
         } catch (IllegalStateException e) {
             // 비정상 처리시 상태코드 400으로 응답 - 클라이언트 잘못
             // 중복 아이디나 중복 이메일 사용시
+            response = ResponseEntity.badRequest().body(e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            // 비정상 처리시 상태코드 500으로 응답
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    // ResponseEntity는 스프링에서 HTTP와 관련된 기능을 구현할때 사용
+    // 상태코드, HTTP헤더, HTTP본문등을 명시적으로 설정 가능
+    @PostMapping ("/login")
+    public ResponseEntity<?> loginok(MemberDTO member, HttpSession session) {
+        // 로그인 처리시 기타오류 발생에 대한 응답코드 설정
+        ResponseEntity<?> response= ResponseEntity.internalServerError().build();
+
+        log.info("submit된 로그인 정보 : {}", member);
+
+        try {
+            // 정상 처리시 상태코드 200으로 응답
+            Member loginUser=memberService.loginMember(member);
+            session.setAttribute("loginUser", loginUser);
+            session.setMaxInactiveInterval(600);        // 세션 유지 : 10분
+
+            response = ResponseEntity.ok().build();
+        } catch (IllegalStateException e) {
+            // 비정상 처리시 상태코드 400으로 응답 - 클라이언트 잘못
+            // 아이디나 비번 잘못 입력시
             response = ResponseEntity.badRequest().body(e.getMessage());
             e.printStackTrace();
         } catch (Exception e) {
