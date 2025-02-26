53e550dab8fae532826dbca1dfdfc97a42bba102
leegwangje <ze105@naver.com>
2025-02-26 12:17:55 +0900

    게시판 list 구현 1 - 계층별 클래스, 인터페이스 추가 BoardDTO, BoardRepository,BoardService, BoardController

---
+++ src/main/java/com/example/semiprojectv1/domain/Board.java
@@ -0,0 +1,19 @@
+package com.example.semiprojectv1.domain;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Board {
+    private int bno;
+    private String title;
+    private String userid;
+    private LocalDateTime regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+}
---
+++ src/main/java/com/example/semiprojectv1/service/BoardServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.semiprojectv1.service;
+
+
+import com.example.semiprojectv1.domain.BoardDTO;
+import com.example.semiprojectv1.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardMapper;
+
+    @Override
+    public List<BoardDTO> readBoard() {
+        return boardMapper.selectBoard();
+    }
+
+}
