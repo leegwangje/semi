b5d98b813c1aa094ae770f6a1d28d1c7c2477266
Kwang je <gwangjae0105@gmail.com>
2025-02-26 12:19:35 +0900

    게시판 list 구현 2 - h2 디비 사용중비 설정, 계층별 테스트 클래스 추가

---
+++ src/test/java/com/example/semiprojectv1/board/BoardMapperTest.java
@@ -0,0 +1,42 @@
+package com.example.semiprojectv1.board;
+
+
+import com.example.semiprojectv1.domain.BoardDTO;
+import com.example.semiprojectv1.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.TestConstructor;
+
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@MybatisTest
+@RequiredArgsConstructor   // final 필드변수를 생성자 생성
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)   // 생성자 주입시 필요
+@AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE)  // H2 데이터베이스 사용중지
+public class BoardMapperTest {
+
+    // autowired가 아닌 생성자를 이용한 의존성 주입이 더 나음
+    private final BoardRepository boardMapper;  // final 제거
+
+    @Test
+    @DisplayName("BoardMapper select test")
+    void selectTest() {
+        // Given : 테스트에 사용할 데이터 제공
+
+
+        // When : 테스트로 테스트할 기능 호출
+        List<BoardDTO> result = boardMapper.selectBoard();
+
+        // Then : 호출되고 난 후 결과값 확인
+        log.info("result: {}", result);  // 변수명 수정 (results → result)
+        assertNotNull(result);
+    }
+}
