150f84a791a6091ad1c546729b5e1982bf3d698e
leegwangje <ze105@naver.com>
2025-02-26 12:19:35 +0900

    게시판 list 구현 2 - h2 디비 사용중비 설정, 계층별 테스트 클래스 추가

---
+++ src/test/java/com/example/semiprojectv1/board/BoardControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.semiprojectv1.board;
+
+
+import com.example.semiprojectv1.repository.BoardRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)   // 생성자 주입시 필요
+public class BoardControllerTest {
+
+    private final MockMvc mockMvc;  // 타입 지정 수정
+    private final BoardRepository boardMapper;
+
+    @Test
+    @DisplayName("/list GET request test")
+    public void list() throws Exception {
+        //Given
+
+        // When
+        mockMvc.perform(get("/board/list"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+
+
+    }
+}
