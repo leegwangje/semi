b0ff80f0973c4e179c26f0725ecf7d3fe7b42edc
Kwang je <gwangjae0105@gmail.com>
2025-02-28 12:21:55 +0900

    게시판 본문글 구현 1 - 본문글 보기 링크 생성, 본문글 출력, 테스트 클래스 작성

--- src/main/java/com/example/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/semiprojectv1/controller/BoardController.java
@@ -41,4 +41,12 @@ public class BoardController {
         m.addAttribute("cntpg", boardService.countfindBoard(findtype,findkey));
         return "views/board/list";
     }
+
+    @GetMapping("/view")
+    public String view(Model m,int bno) {
+
+        m.addAttribute("bd", boardService.readOneBoard(bno));
+
+        return "views/board/view";
+    }
 }
