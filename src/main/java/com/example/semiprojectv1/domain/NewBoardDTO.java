c68bf1ca8bb9c2939086463577d5ba408a92d555
Kwang je <gwangjae0105@gmail.com>
2025-03-04 12:31:02 +0900

    게시판 글쓰기 구현 3  - reCaptcha 검증 서비스 구현 NewBoardDTO 구현 RestTemplate 빈즈 정의

---
+++ src/main/java/com/example/semiprojectv1/domain/NewBoardDTO.java
@@ -0,0 +1,17 @@
+package com.example.semiprojectv1.domain;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NewBoardDTO {
+
+    private String title;
+    private String userid;
+    private String contents;
+
+}
