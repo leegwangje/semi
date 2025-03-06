8eb030976afed7fbabab688d37619935324e51e0
leegwangje <ze105@naver.com>
2025-03-06 12:45:16 +0900

    겔러리 구현 2 - Gallery 클래스 정의 GalleryService, GalleryRepository 구현

---
+++ src/main/java/com/example/semiprojectv1/domain/Gallery.java
@@ -0,0 +1,20 @@
+package com.example.semiprojectv1.domain;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Gallery {
+    private int gno;
+    private String title;
+    private String userid;
+    private LocalDateTime regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String simgname;
+}
---
+++ src/main/java/com/example/semiprojectv1/service/GalleryService.java
@@ -0,0 +1,12 @@
+package com.example.semiprojectv1.service;
+
+import com.example.semiprojectv1.domain.Gallery;
+
+import java.util.List;
+
+public interface GalleryService {
+
+    List<Gallery> selectGallery();
+
+
+}
