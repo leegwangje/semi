63588c975c1f0543daf592768b70d7113869546b
leegwangje <ze105@naver.com>
2025-04-01 10:13:55 +0900

    로그인 처리 시 응답코드 및 본문 재설정

--- src/main/java/com/example/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -2,11 +2,16 @@ package com.example.semiprojectv1.controller;
 
 import com.example.semiprojectv1.domain.Member;
 import com.example.semiprojectv1.domain.MemberDTO;
+import com.example.semiprojectv1.jwt.JwtTokenProvider;
 import com.example.semiprojectv1.repository.MemberRepository;
 import com.example.semiprojectv1.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -15,6 +20,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Slf4j
@@ -24,6 +32,8 @@ import javax.servlet.http.HttpSession;
 public class MemberController {
 
     private final MemberService memberService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/join")
     public String join() {
@@ -83,40 +93,59 @@ public class MemberController {
         }
         return response;
     }
-        // 스프링 시큐리티가 자동으로 처리 - 생략
-    // ResponseEntity는 스프링에서 HTTP와 관련된 기능을 구현할때 사용
-    // 상태코드, HTTP헤더, HTTP본문등을 명시적으로 설정 가능
-//    @PostMapping ("/login")
-//    public ResponseEntity<?> loginok(MemberDTO member, HttpSession session) {
-//        // 로그인 처리시 기타오류 발생에 대한 응답코드 설정
-//        ResponseEntity<?> response= ResponseEntity.internalServerError().build();
-//
-//        log.info("submit된 로그인 정보 : {}", member);
-//
-//        try {
-//            // 정상 처리시 상태코드 200으로 응답
-//            Member loginUser=memberService.loginMember(member);
-//            session.setAttribute("loginUser", loginUser);
-//            session.setMaxInactiveInterval(600);        // 세션 유지 : 10분
-//
-//            response = ResponseEntity.ok().build();
-//        } catch (IllegalStateException e) {
-//            // 비정상 처리시 상태코드 400으로 응답 - 클라이언트 잘못
-//            // 아이디나 비번 잘못 입력시
-//            response = ResponseEntity.badRequest().body(e.getMessage());
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            // 비정상 처리시 상태코드 500으로 응답
-//            e.printStackTrace();
-//        }
-//        return response;
-//    }
+//         스프링 시큐리티가 자동으로 처리 - 생략
+//     ResponseEntity는 스프링에서 HTTP와 관련된 기능을 구현할때 사용
+//     상태코드, HTTP헤더, HTTP본문등을 명시적으로 설정 가능
+    @PostMapping ("/login")
+    public ResponseEntity<?> loginok(MemberDTO member, HttpServletResponse res) {
+        // 로그인 처리시 기타오류 발생에 대한 응답코드 설정
+        ResponseEntity<?> response= ResponseEntity.internalServerError().body("서버처리시 오류가 발생했습니다!!");
+
+        log.info("submit된 로그인 정보 : {}", member);
+
+        try {
+            // 스프링 시큐리티에서 제공하는 인증처리 메니저로 인증 처리
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(member.getUserid(),member.getPasswd())
+            );
+            // 인증이 완료되면 jwt 토큰 생성
+            final String jwt = jwtTokenProvider.generateToken(member.getUserid());
+
+            // JWT 토큰을 쿠키에 저장
+            Cookie cookie = new Cookie("jwt", jwt);
+            cookie.setHttpOnly(true); // 토큰은 header를 통해서만 서버로 전송가능
+            cookie.setMaxAge(60 * 30); // 30분
+            cookie.setPath("/");
+            res.addCookie(cookie);
+
+            response = ResponseEntity.ok().body("로그인 성공했습니다!!");
+        } catch (BadCredentialsException e) {
+            // 인증 실패시  상태코드 401으로 응답 - 아이디나 비밀번호 잘못 입력시
+            // 아이디나 비번 잘못 입력시
+            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("아이디나 비밀번호를 확인하세요");
+            log.info(e.getMessage());
+        } catch (Exception e) {
+            // 비정상 처리시 상태코드 500으로 응답
+            log.info(e.getMessage());
+        }
+        return response;
+    }
     // 스프링 시큐리티가 자동으로 처리 - 생략
-//    @GetMapping("/logout")
-//    public String logout(HttpSession session) {
-//        session.invalidate();   // 세션 제거
-//
-//        return "redirect:/";
-//    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest req, HttpServletResponse res) {
+        // 쿠키에서 JWT 삭제
+        Cookie cookie = new Cookie("jwt", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        res.addCookie(cookie);
+        // 세션 무효화
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
 
 }
